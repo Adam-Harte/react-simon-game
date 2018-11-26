6d32027cc55ddad83519cf3b1308b596711372da
Adam-Harte <adamharte11@hotmail.co.uk>
2018-11-26 21:15:50 +0000

    styles added for different button types and applied as a joined string of array items to the button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 const Button = (props) => {
+   const classes = ['Button', props.btnType].join(' ');
    return (
-      <button onClick={props.clicked}>{props.children}</button>
+      <button className={classes} onClick={props.clicked}>{props.children}</button>
    );
 };
 
